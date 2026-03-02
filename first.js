233fbf4aa6c54efa16d6d35610ba1de422e967cc
Abhijeet Sharma <abhijeetboss9696@gmail.com>
2026-03-02 23:58:08 +0530

    rebase added

--- first.js
+++ first.js
@@ -4,13 +4,10 @@ console.log(button);
 const login = "login page added";
 console.log(login);
 
-<<<<<<< HEAD
 const footer = ("Footer added in our website.. ");
 console.log(footer);
-=======
 const payment = "integrated the payment gatway";
 console.log(payment);
 
 const upi = ("integrated the UPI");
 console.log(upi);
->>>>>>> 3d5e8af (upi added)
